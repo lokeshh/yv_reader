9c629a3cf79251193faf4e59b52a6c54a6d84c28
Lokesh Sharma <lokeshhsharma@gmail.com>
2021-10-26 17:20:39 +0530

    working

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import { Container } from '@mui/material'
+
+class App extends Component {
+  render() {
+    return <div>
+      <Container>
+        <center><h1>योगवासिष्ठ</h1></center>
+      </Container>
+    </div>
+  }
+}
+
+export default App
